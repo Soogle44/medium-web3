45594f0281597fd4ac96e6eba43f68a9705cf25d
Soogle44 <hsgru3@gmail.com>
2022-08-28 17:04:01 +0900

    add upload metadata function

--- src/pages/NewStory.js
+++ src/pages/NewStory.js
@@ -1,13 +1,89 @@
 import { useState } from "react";
 import "./NewStory.css"
+import {
+  useMoralisFile,
+  useMoralis,
+  useWeb3ExecuteFunction,
+} from "react-moralis";
 
 
 const NewStory = () => {
 
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const { saveFile } = useMoralisFile();
+  const { Moralie, account } = useMoralis();
+  const contractProcessor = useWeb3ExecuteFunction();
+
+  const uploadFile = async (event) => {
+    event.preventDefault();
+    const textArray = text.split();
+    const metadata = {
+      title,
+      text: textArray,
+    };
+
+    try {
+      const result = await saveFile(
+        "myblog.json",
+        { base64: btoa(JSON.stringify(metadata)) },
+        {
+          type: "base64",
+          saveIPFS: true,
+        }
+      );
+      const nftResult = await uploadNftMetadata(result.ipfs());
+
+      alert(nftResult.ipfs())
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  const uploadNftMetadata = async (url) => {
+    const metadataNft = {
+      image: "",
+      description: title,
+      externalUrl: url,
+    };
+    const resultNft = await saveFile(
+      "metadata.json",
+      { base64: btoa(JSON.stringify(metadataNft)) },
+      {
+        type: "base64",
+        saveIPFS: true,
+      }
+    );
+    return resultNft;
+  }
+
   return (
     <>
       <div>
-
+        <form onSubmit={uploadFile} className="writeForm">
+          <div className="writeFormGroup">
+            <input
+              className="writeInput"
+              placeholder="Title"
+              type="text"
+              autoFocus={true}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="writeFormGroup">
+            <textarea
+              className="writeInput writeText"
+              placeholder="Tell your story..."
+              autoFocus={true}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+          <button className="writeSubmit" type="submit">
+            Publish
+          </button>
+        </form>
       </div>
     </>
   );
